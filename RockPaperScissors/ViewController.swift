e912ed704a7843f39828b8c011bb826c123f40ec
Artyom Gurbovich <artyom.gurbovich@icloud.com>
2019-11-04 01:37:36 +0300

    Initial Commit

---
+++ RockPaperScissors/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RockPaperScissors
+//
+//  Created by Artyom Gurbovich on 11/4/19.
+//  Copyright © 2019 Artyom Gurbovich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
